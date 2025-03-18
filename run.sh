1acbd41a39014aa137afd81445064534dfbd2a07
ravilock <ravi.me.professional@hotmail.com>
2025-03-18 14:38:05 -0300

    chore: improve run script

--- run.sh
+++ run.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
+# Compile the Java program
 javac JogoDoMarciano.java
-java -classpath ".:sqlite-jdbc-3.49.1.0.jar" JogoDoMarciano
+
+# Check if the operating system is Windows or Linux/Mac
+OS=$(uname -s)
+
+if [[ "$OS" == "Linux" || "$OS" == "Darwin" ]]; then
+  # On Linux or macOS, use colon ":" as classpath separator
+  java -classpath ".:sqlite-jdbc-3.49.1.0.jar" JogoDoMarciano
+elif [[ "$OS" == "CYGWIN"* || "$OS" == "MINGW"* || "$OS" == "MSYS"* ]]; then
+  # On Windows (Cygwin, MinGW, MSYS), use semicolon ";" as classpath separator
+  java -classpath ".;sqlite-jdbc-3.49.1.0.jar" JogoDoMarciano
+else
+  echo "Unsupported OS: $OS"
+  exit 1
+fi
